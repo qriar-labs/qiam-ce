1143551698e32c44c7da99043d20c0630ed2fe13
Edgar Silva <edgar@skalena.com>
2024-04-10 02:03:24 -0300

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://qriar-labs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: 'qiam-ce',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'qriarlabs', // Usually your GitHub org/user name.
-  projectName: 'qiam', // Usually your repo name.
+  organizationName: 'qriar-labs', // Usually your GitHub org/user name.
+  projectName: 'qiam-ce', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
